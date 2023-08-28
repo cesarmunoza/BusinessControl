5b66a7842ffa16bbfd2d7feb881a131e764c2ff7
cesar <cesare.munoz@gmail.com>
2023-08-28 10:58:15 -0500

    Se agregaron controllers para el home y para la pagina de producto. Tambien se configuraron las vistas correspondientes

---
+++ src/main/java/com/negocio/app/controllers/ProductoController.java
@@ -0,0 +1,24 @@
+package com.negocio.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.negocio.app.models.dao.IProductoDao;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired
+	private IProductoDao productoDao;
+	
+	@RequestMapping(value="/listar", method=RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo", "Listado de productos");
+		model.addAttribute("productos", productoDao.findAll());
+		return "listarProductos";
+	}
+
+}
