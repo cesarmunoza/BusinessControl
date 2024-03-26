8029755a75f213fba0810409268c008baeab503f
cesar <cesare.munoz@gmail.com>
2024-03-26 01:42:04 -0500

    Se implementa el método guardar en FacturaController, para guardad la factura en bd

--- src/main/java/com/negocio/app/controllers/FacturaController.java
+++ src/main/java/com/negocio/app/controllers/FacturaController.java
@@ -9,15 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.negocio.app.models.entity.Cliente;
 import com.negocio.app.models.entity.Factura;
+import com.negocio.app.models.entity.ItemFactura;
 import com.negocio.app.models.entity.Producto;
 import com.negocio.app.models.service.IClienteService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/factura")
 @SessionAttributes("factura")
@@ -51,9 +57,31 @@ public class FacturaController {
 		return clienteService.findByNombre(term);
 	}
 	
-	@PostMapping("/form")
-	public String guardar(Factura factura) {
-		return "";
+	@PostMapping("/formFactura")
+	public String guardar(Factura factura,
+			@RequestParam(name = "item_id[]", required =false) Long[] itemId,
+			@RequestParam(name = "cantidad[]", required = false) Integer[] cantidad,
+			RedirectAttributes flash,
+			SessionStatus status) {
+		
+		for (int i = 0; i < itemId.length; i++) {
+			Producto producto = clienteService.findProductoById(itemId[i]);
+			
+			ItemFactura linea = new ItemFactura();
+			linea.setCantidad(cantidad[i]);
+			linea.setProducto(producto);
+			
+			factura.addItemFactura(linea);
+			
+			log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
+		}
+		
+		clienteService.saveFactura(factura);
+		status.setComplete();
+		
+		flash.addFlashAttribute("Success", "Factura creada con éxito!");
+		
+		return "redirect:/ver/" + factura.getCliente().getIdCliente();
 	}
 
 }
